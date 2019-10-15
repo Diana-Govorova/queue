e2602f9d700bf619b08a5e2cac795752d7e749b0
Diana-Govorova <diana.govorova2001@gmail.com>
2019-10-15 16:42:16 +0300

    Написаны объявления функций

--- queue(очередь)/queue(очередь).cpp
+++ queue(очередь)/queue(очередь).cpp
@@ -1,5 +1,32 @@
 ﻿#include <stdio.h>
 
+struct QueueElement 
+{
+	QueueElement* next;
+	int value;
+};
+
+struct Queue
+{
+	QueueElement* head;
+	QueueElement* tail;
+};
+
+bool isEmpty(Queue *queue)
+{
+
+}
+
+void enqueue(Queue *queue, int value)
+{
+
+}
+
+int dequeue(Queue *queue)
+{
+
+}
+
 int main()
 {
 
